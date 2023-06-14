72c20cebb26c78ee8fdc5d22f998a2ebb4a679f5
Abraham.Lugonzo <abramlugonzo@gmail.com>
2023-06-14 15:56:17 +0300

    switch test

--- src/test/java/com/example/demo/TestCases.java
+++ src/test/java/com/example/demo/TestCases.java
@@ -4,14 +4,23 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
 public class TestCases {
 
 
-    @Test
-    void switchCaseTest(){
-       // assertEquals();
+    @ParameterizedTest()
+    @ValueSource(strings = "BMW")
+    void switchCaseTest(String brand){
+
+        switch (brand){
+            case "BMW","BMW2","BMW3" -> System.out.println("BMW");
+            case "TOYOTA","TOYOTA1" -> System.out.println("TOYOTA");
+            case "VOLVO" -> System.out.println("TOYOTA");
+            default -> System.out.println("AUDI");
+        }
     }
 }
